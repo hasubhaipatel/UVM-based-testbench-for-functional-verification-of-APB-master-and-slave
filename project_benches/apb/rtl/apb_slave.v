b36b830f1ec5153ee5dbd8582d908e800d480084
idhriti10 <iddya.dhriti@gmail.com>
2019-07-01 23:27:00 -0400

    Slave verified at UNIT level

--- project_benches/apb/rtl/apb_slave.v
+++ project_benches/apb/rtl/apb_slave.v
@@ -1,20 +1,20 @@
 /***************            Created by : Dhriti Iddya                    *******************/
 /*************** Verilog module for implementation of APB slave protocol *******************/
 
-module APB_SLAVE(PRESET,PCLK,PSEL,PENABLE,PREADY,PADDR,PWRITE,PWDATA,PRDATA);
+module apb_slave(PRESET,PCLK,PSEL,PENABLE,PREADY,PADDR,PWRITE,PWDATA,PRDATA);
 	
-	parameter ADDR_WIDTH;
-	parameter DATA_WIDTH;
+	parameter ADDR_WIDTH = 8;
+	parameter DATA_WIDTH = 32;
 	parameter [1:0]
-		IDLE    = 2'b00;
-		WRITE   = 2'b01;
-		READ    = 2'b10;
+		IDLE    = 2'b00,
+		WRITE   = 2'b01,
+		READ    = 2'b10,
 		ILLEGAL = 2'b11;
 
 	//-------------------------- Input signals ------------------------//
 	
 	input PRESET;
-	input CLK;
+	input PCLK;
 	input PSEL;
 	input PENABLE;
 	input PWRITE;
@@ -28,29 +28,30 @@ module APB_SLAVE(PRESET,PCLK,PSEL,PENABLE,PREADY,PADDR,PWRITE,PWDATA,PRDATA);
 
 	//------------------------- Nets and Registers ----------------------//
 
-	reg [1:0] current_state,next_state;
-	reg [DATA_WIDTH-1:0] RAM [0:2**ADDR_WIDTH-1];  // Models storage in the slave for reading/writing data
-	
+  reg [1:0] current_state,next_state = 0;
+  reg [DATA_WIDTH-1:0] RAM [0:2**ADDR_WIDTH-1];  // Models storage in the slave for reading/writing data
+    	
 	//------------------------- Sequential Logic -------------------------//
 	
 	always@(posedge PCLK or negedge PRESET)
 	begin
-		if(!PRESET)
+      if(!PRESET)
 			current_state <= IDLE;
 		else
 			current_state <= next_state;
 	end
 	
 	//--------------------------- Next State and Output Logic -----------------------//
-	always@(current_state)
+  always@(*)
 	begin
-	PRDATA = 0, PREADY = 0;
+    PRDATA = 0;
+    PREADY = 0;
 	case (current_state)
 		IDLE: begin
-			if(PSEL && PENABLE) begin
+          if(PSEL && !PENABLE) begin
 				if(PWRITE)
 					next_state = WRITE;
-				else if (!WRITE)
+              else if (!PWRITE)
 					next_state = READ;
 			end
 		end
@@ -60,15 +61,15 @@ module APB_SLAVE(PRESET,PCLK,PSEL,PENABLE,PREADY,PADDR,PWRITE,PWDATA,PRDATA);
 				PREADY = 1;
 				RAM[PADDR] = PWDATA;
 			end
-			next_state = IDLE;
+          	next_state = IDLE;
 		end
 		
 		READ: begin
-			else if (PSEL && PENABLE && !PWRITE) begin
+			if (PSEL && PENABLE && !PWRITE) begin
 				PREADY = 1;
-				PRDATA = RAM[PADDR];			
+				PRDATA = RAM[PADDR];
 			end
-			next_state = IDLE;
+          	next_state = IDLE;
 		end
 		
 		ILLEGAL: begin
@@ -76,6 +77,7 @@ module APB_SLAVE(PRESET,PCLK,PSEL,PENABLE,PREADY,PADDR,PWRITE,PWDATA,PRDATA);
 		end
 		
 		default: next_state = IDLE;
-	end
+    endcase
+    end
 
 endmodule
